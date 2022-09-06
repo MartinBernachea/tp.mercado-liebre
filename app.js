355c43b8d26c674e7e308aebc25c698d6f3c4a7c
martin berna <bernacheamartin@outlook.com>
2022-09-06 12:50:37 -0300

    Agrego .gitignore , Actualizo html +  CSS

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var app = express();
 
 var publicPath = path.join(__dirname, '/public');
 
+app.use(express.static(publicPath));
+
+app.get('/css', function (req, res) {
+    res.sendFile(__dirname + '/public/assets/css/stylesheet.css');
+});
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname, '/views/index.html')
-})
+    res.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
-})
+});

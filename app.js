417b21df8f568a0e2b8244e90a256e57d1e50a9a
MartinBernachea <bernacheamartin@outlook.com>
2022-09-09 04:54:13 -0300

    Agrego form-register.html + CSS + responsive

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/views/pages/form-login.html');
 });
 
+app.get('/register', function (req, res) {
+    res.sendFile(__dirname + '/views/pages/form-register.html');
+});
+
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
 });
